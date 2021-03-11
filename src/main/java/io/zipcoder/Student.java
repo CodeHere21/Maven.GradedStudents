31896f7f0611caa4edbd6cfabb0959a01f8bdf50
lena <alena.litouka@gmail.com>
2021-03-10 21:09:52 -0500

    dgjrg

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,35 @@
 package io.zipcoder;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Student {
+
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores;
+
+    //Default Nullery Constructor
+    public Student(){
+        firstName="";
+        lastName="";
+        examScores= new ArrayList<Double>(3);
+        examScores.add(100.0);
+        examScores.add(90.0);
+        examScores.add(130.0);
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public String getExamScores(){
+        String tempString="";
+        tempString+=examScores.toString();
+        System.out.println(tempString);
+        return tempString;
+    }
+
 }
